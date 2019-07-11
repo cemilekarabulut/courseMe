19b81504ef5d34e5c88abf047d7ab40bdc0aa08d
cemilekarabulut <cemile.karabulut@metu.edu.tr>
2019-07-11 09:27:42 +0300

    second commit

--- course app/Controllers/CourseController.cs
+++ course app/Controllers/CourseController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using course_app.Models;
- using System.Threading.Tasks;
+using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 
 namespace course_app.Controllers
@@ -16,36 +16,32 @@ namespace course_app.Controllers
         [HttpPost]
         public ActionResult Index(string button)
         {
-                return View("Login");
-           
+            return View("Login");
+
         }
         public ActionResult Index()
         {
             return View();
+            
 
-           
         }
-        public ActionResult Login()
-        {
-    
-            return View();
-        }
+        
         [HttpPost]
         public ActionResult Login(login a)
         {
-            string str="";
-                conn.Open();
-                MySqlCommand cmd = new MySqlCommand($"select email,title from login where email='{a.email}' and password='{a.password}';", conn);
+            string str = "";
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand($"select email,title from login where email='{a.email}' and password='{a.password}';", conn);
 
-                MySqlDataReader MyReader;
-                MyReader = cmd.ExecuteReader();
+            MySqlDataReader MyReader;
+            MyReader = cmd.ExecuteReader();
             while (MyReader.Read())
             {
                 Console.WriteLine(MyReader["title"].ToString());
-              str=MyReader["title"].ToString();
+                str = MyReader["title"].ToString();
             }
-      
-                conn.Close();
+
+            conn.Close();
             if (String.IsNullOrEmpty(str))
                 return View("error");
             else if (str == "student")
@@ -54,12 +50,12 @@ namespace course_app.Controllers
                 return View("instructor");
             else if (str == "registrar")
                 return View("registrar");
-            
+
             else
                 return View("parent");
         }
-           
-        
+
+
         public ActionResult error()
         {
             return View();
@@ -68,10 +64,10 @@ namespace course_app.Controllers
 
         public ActionResult error(string str)
         {
-            return View("Login");
+            return View("Index");
         }
-       
-     
+
+
         public ActionResult student()
         {
             return PartialView();
@@ -108,40 +104,52 @@ namespace course_app.Controllers
         {
             return View();
         }
-        
+        [AcceptVerbs(HttpVerbs.Get)]
+
         public ActionResult Signup()
+        {
+
+                return View();
+     
+        }
+
+      
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Signup(student user)
+        {
+
+           
+                conn.Open();
+            MySqlCommand cmd = new MySqlCommand($"insert into student(name,surname,email,password,age,gender) values('{user.name}', '{user.surname}', '{user.email}', '{user.password}', '{user.age}', '{user.gender}');", conn);
+                MySqlCommand cmd2 = new MySqlCommand($"insert into login(email,password,title) values( '{user.email}', '{user.password}', 'student');", conn);
+                MySqlDataReader MyReader;
+            MyReader = cmd.ExecuteReader();
+            conn.Close();
+            conn.Open();
+            MyReader = cmd2.ExecuteReader();
+            conn.Close();
+
+
+
+            return View("Index");
+
+        }
+        public ActionResult info()
+        {
+            return View();
+                
+        }
+        public ActionResult addCourse()
         {
             return View();
         }
-
-    [HttpPost]
-
-        public void Signup(parent user)
+        public ActionResult grades()
         {
-
-            //if (user.studentId != 0)
-            //{
-
-            //    conn.Open();
-            //    MySqlCommand cmd = new MySqlCommand($"insert into parent(name,surname,email,password,age,gender,studentId) values('{user.name}', '{user.surname}', {user.email}', '{user.password}', '{user.age}', '{user.gender}','{user.studentId}');", conn);
-
-            //    MySqlDataReader MyReader;
-            //    MyReader = cmd.ExecuteReader();
-
-            //    conn.Close();
-
-            //}
-            //else
-            //{
-                conn.Open();
-                MySqlCommand cmd = new MySqlCommand($"insert into student(name,surname,email,password,age,gender) values('{user.name}', '{user.surname}', '{user.email}');", conn);
-
-                MySqlDataReader MyReader;
-                MyReader = cmd.ExecuteReader();
-
-                conn.Close();
-            //}
-         
+            return View();
+        }
+        public ActionResult showStudent()
+        {
+            return View();
         }
     }
 }
